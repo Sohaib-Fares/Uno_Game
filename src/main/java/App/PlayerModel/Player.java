4c345c55ae8e6ed2e4997b900dc5b14377c3773b
Abdelaziz <zizoularaba91@gmail.com>
2024-12-18 23:11:56 +0100

    change on player class

--- src/main/java/App/PlayerModel/Player.java
+++ src/main/java/App/PlayerModel/Player.java
@@ -1,4 +1,38 @@
 package App.PlayerModel;
 
+import java.util.ArrayList;
+import App.CardModel.AbstractCard;
+
+
 public abstract class Player {
+private String name;
+ArrayList<AbstractCard> hand = new ArrayList<>();
+
+public void AddCard(Deck Deck2){
+    hand.add(Deck2.get(Deck2.size()));
+    Deck2.remove(Deck2.size());
 }
+public boolean HasCard(){
+        return !hand.isEmpty();
+    }
+
+
+
+
+public String getName() {
+    return name;
+}
+public void setName(String Name) {
+    name = Name;
+}
+public ArrayList<AbstractCard> getHand() {
+    return hand;
+}
+public void setHand(ArrayList<AbstractCard> Hand) {
+    hand = Hand;
+}
+public String toString() {
+    return name;
+}
+}
+
