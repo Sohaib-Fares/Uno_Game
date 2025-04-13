81c1b4c661a2c0d88e12e71a1bb0839d523ac385
Imad-Belatreche <imad.belatreche@etu.usthb.dz>
2025-04-13 18:20:44 +0100

    Updated with the logo and BaseFrame

--- src/main/java/UI/Components/Frames/MuMainFrame.java
+++ src/main/java/UI/Components/Frames/MuMainFrame.java
@@ -5,6 +5,8 @@ import javax.swing.JFrame;
 public class MuMainFrame extends JFrame {
     public MuMainFrame() {
         super();
+        super.setSize(900, 900);
+        super.setVisible(true);
     }
 
 }
