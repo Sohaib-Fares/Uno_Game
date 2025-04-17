8f8b14b262199f91e79f84fb6744877a3a8dd0d0
Imad-Belatreche <imad.belatreche@etu.usthb.dz>
2025-04-17 13:51:23 +0100

    Generated Cards (Not resizable / Still gigity giged up)

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,12 +1,12 @@
 import java.io.IOException;
-import AppService.Game.GameService;
 import UI.Components.Frames.MuMainFrame;
 
 public class MainApp {
     public static void main(String[] args) throws IOException {
         // GameService service = new GameService();
         // service.showGameMenu();
+        // ...
         new MuMainFrame();
-        
+
     }
 }
