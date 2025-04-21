7e7239e0f31e9dd9ae75e7f2b050f4bde4c2938b
Larabaabdelaziz <zizoularaba91@gmail.com>
2025-04-21 18:34:09 +0100

    hna aktb summary 3la wach drt

--- src/main/java/UI/Components/Panels/MuPanel.java
+++ src/main/java/UI/Components/Panels/MuPanel.java
@@ -2,8 +2,11 @@ package UI.Components.Panels;
 
 import javax.swing.JPanel;
 
+import org.eclipse.jdt.core.dom.Dimension;
+
 public class MuPanel extends JPanel {
     public MuPanel() {
         super();
     }
 }
+
